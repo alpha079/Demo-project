e6661eccc70c3f987d4bbdeefd22fccb91aa350c
Shivam Soni <shivam.soni@nagarro.com>
2020-04-16 22:53:37 +0530

    Final Commit

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ const todoRoute = require('./routes/todos')
 const methodOverride = require('method-override') 
 app.use(express.urlencoded({ extended: true }))
 app.use(express.json())
+var PORT = process.env.PORT || 5000
 app.set("view engine","ejs");
 
 
@@ -34,7 +35,7 @@ app.patch('/:id', async(req, res) => {
 
 db.sync()
 .then(() => {
-    app.listen(5000)
+    app.listen(PORT)
 }).catch((err) => {
     console.error(err)
 })
